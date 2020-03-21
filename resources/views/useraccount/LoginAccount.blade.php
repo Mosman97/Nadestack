82991614a320e0cf52a45d35e5cf71ac9c45f0d4
Eric <39589340+Mosman97@users.noreply.github.com>
2020-03-21 18:34:27 +0100

    login seite

--- resources/views/useraccount/LoginAccount.blade.php
+++ resources/views/useraccount/LoginAccount.blade.php
@@ -8,10 +8,10 @@
     <div class="row">
         <div class="col-xl-2"></div>
         <div class="col">
-            <div id="login_form_container" style="padding-bottom: 0px;width:auto;height: auto;padding-top: 0px;padding-left: 00px; margin-left: auto;">
+            <div id="login_form_container" style="width:auto;height: auto;margin-left: auto;">
                 <!-- Start: Pretty Registration Form -->
                 <div class="row text-white register-form" style="color: rgb(255,255,255);">
-                    <div class="col-md-8 col-md-offset-4">
+                    <div class="col-md-8 offset-md-2">
                         <form class="text-white nadestack_form" id="registration_form" style="color: rgb(255,255,255);"action="{{ route('login')}}"  method="POST">
                             @csrf
                             <h1 class="text-center text-light nadestack_heading_one">Login</h1>
@@ -28,7 +28,7 @@
                             </div>
                             <div class="form-row form-group">
                                 <div class="col-sm-4 col-xl-3 text-center d-xl-flex justify-content-xl-center align-items-xl-center label-column"><label class="col-form-label">Password:</label></div>
-                                <div class="col-sm-6 input-column"><input  id="password" class="form-control" type="password"name="password"></div>
+                                <div class="col-sm-6 input-column"><input  id="password" class="form-control" type="password"name="password" placeholder="Enter Password"></div>
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -40,32 +40,16 @@
                                 <div class="col-md-12 text-center" style="padding-bottom: 15px; padding-top: 15px;"><button class="btn nadestack_btn" type="submit">Login</button></div>
                             </div>
                             <hr class="bg-light"/>
-                            <div class="form-row form-group text-center">
-                                <div class="col-md-6"><a href="{{route('account.usernameforget')}}">Forgot Username?</a></div>
+                            <div class="form-row" style="padding-bottom: 15px">
+                                <div class="col-md-12 text-center"><a href="{{route('account.usernameforget')}}">Forgot Username?</a></div>
                             </div>
-                            <div class="form-row form-group text-center">
-                                <div class="col-md-6"><a href="{{url('/password/reset')}}">Forgot Password?</a></div>
+                            <div class="form-row">
+                                <div class="col-md-12 text-center nadestack-last-element"><a href="{{url('/password/reset')}}">Forgot Password?</a></div>
                             </div>
                         </form>
                     </div>
                 </div>
-                <div class="row">
-                             <div class="form-row">
-                                <div class="col"></div>
-                            </div>
-                </div>
-                @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
-                <!-- End: Pretty Registration Form -->
             </div>
-
         </div>
         <div class="col-xl-2"></div>
     </div>
