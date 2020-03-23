0ee112425d7d7b350549d6c9165541daa8319a9e
gabrieldickert <59055689+gabrieldickert@users.noreply.github.com>
2020-03-23 18:53:54 +0100

    Added TeamLink to Profilepage

--- app/Http/Controllers/UserprofilePageController.php
+++ app/Http/Controllers/UserprofilePageController.php
@@ -5,46 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\User;
+
 class UserprofilePageController extends Controller {
-    
-    
-    
+
     /**
      * 
      * @param Request $request 
      * @param type $userid the UserID
      */
     function getUserProfileData(Request $request, $username) {
-        
-        
+
+
         //Searching User
-        
-       $user_exits =  User::where("username","=",$username)->count();
-        
-        
-        if($user_exits == 1) {
-            
-            
-            
-             $user_data =  User::where("username","=",$username)->get();
-            
-            
-               return view("useraccount.profile")->with("userdata",$user_data);
-            
-        }
-        
-        else {
-            
+
+        $user_exits = User::where("username", "=", $username)->count();
+
+
+        if ($user_exits == 1) {
+
+
+
+            $user_data = User::where("username", "=", $username)
+                    ->leftJoin('teams', 'users.team_id', '=', 'teams.team_id')
+                    ->select('users.*', 'teams.team_name')
+                    ->get()
+                   ;
+
+
+            return view("useraccount.profile")->with("userdata", $user_data);
+        } else {
+
             echo("USER NOT FOUND");
         }
-        
-        
-        
-        
-        
-     
-    
-
     }
 
 }
