c845c7ea8466208a9edf522e2497de9f5910d48e
Luke <lukas.von.loefen@gmail.com>
2020-03-29 22:55:14 +0200

    New Leagueoverview: 3 Views:  - 1. season_overview.blade.php for selecting the specific season e.g. season 1  - 2. division_overview.blade.php for selecting a divisiontyp e.g. starter division  - 3. group_overview.blade.php for selecting the specific group from a divisiontyo e.g. starter division -> starter division 5

--- resources/views/league/Leagueoverview/season_overview.blade.php
+++ resources/views/league/Leagueoverview/season_overview.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-xl-3 "></div>
             <div class="col-xl-6 colum_content_big" style="background-color: #474B4F">
-                <h1 class="text-center nadestack_heading_one"><img id='nav_logo' src='{{URL::asset('assets/img/logo.png')}}' width="60px"/>Nadestack League</h1>
+                <h1 class="text-center nadestack_heading_one"><img id='nav_logo' src="assets/img/logo.png" width="60px"/>Nadestack League</h1>
                 <hr style="background-color: #222629; height:1px; border:none;color: #222629 "/>
                 <h2 class="text-center nadestack_heading_two">Timetable - Season 1</h2>
                 <div class="row">
@@ -45,14 +45,46 @@
                 <h2 class="text-center nadestack_heading_two">Find Division</h2>
                 <div class="row">
                     <div class="col">
-                        <ul class="breadcrumb" style="background-color: #222629">
-                            <li class="breadcrumb-item"><a href="#">League</a></li>
-                            <li class="breadcrumb-item"><a href="#">Season 1</a></li>
-                            <li class="breadcrumb-item active" aria-current="page" style="color: #86C232">Starter Division</li>
-                        </ul>
+                        <nav aria-label="breadcrumb">
+                            <ul class="breadcrumb" style="background-color: #222629">
+                                <li class="breadcrumb-item active" aria-current="page" style="color: #86C232">League</li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
-            <div class="col"></div>
+                <div class="row">
+                    <div class="col">
+                        <div class="table-responsive">
+                            <table class="table nadestack-tbl table-sm table-borderless">
+                                <thead>
+                                <tr>
+                                    <td>Time</td>
+                                    <td>Season</td>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr style="background-color: #303436">
+                                    <td>05.03.2020 - 05-30-20</td>
+                                    <td><a href="leagueoverview_season.html">Season 1</a></td>
+                                </tr>
+                                <tr>
+                                    <td>05.03.2020 - 05-30-20</td>
+                                    <td><a href="leagueoverview_season.html">Season 1</a></td>
+                                </tr>
+                                <tr style="background-color: #303436">
+                                    <td>05.03.2020 - 05-30-20</td>
+                                    <td><a href="leagueoverview_season.html">Season 1</a></td>
+                                </tr>
+                                <tr>
+                                    <td>05.03.2020 - 05-30-20</td>
+                                    <td><a href="leagueoverview_season.html">Season 1</a></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
