1da888b4f5775b9fb6d25b8cfd08fed437775d72
Luke <lukas.von.loefen@gmail.com>
2020-03-31 18:50:54 +0200

    Added 404 Site

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,17 @@
-@extends('errors::minimal')
+@extends('templates.default_template')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-3"></div>
+            <div class="col-xl-6 colum_content_big text-center" style="background-color: unset">
+                    <img src='{{URL::asset('assets/img/404.jpeg')}}' alt="404-Page not found" style="width: 100%;">
+                    <!--<div class="row">
+                        <div class="col text-right"><button class="btn btn-primary nadestack_btn" type="button" style="margin-bottom: 15px">Go Home</button></div>
+                        <div class="col text-left"><button class="btn btn-primary nadestack_btn" type="button" style="margin-bottom: 15px">Contact Us</button></div>
+                    </div>-->
+            </div>
+            <div class="col-xl-3"></div>
+        </div>
+    </div>
+@endsection
