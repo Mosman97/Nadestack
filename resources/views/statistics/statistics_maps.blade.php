0ecf3251009d4c075bf9b332ceac0f358319f0f3
Eric <39589340+Mosman97@users.noreply.github.com>
2020-05-01 11:55:27 +0200

    implementierung cdn charts

--- resources/views/statistics/statistics_maps.blade.php
+++ resources/views/statistics/statistics_maps.blade.php
@@ -1,47 +1,54 @@
 @extends('templates.default_template')
 
 @section('content')
+    <script src="https://www.jsdelivr.com/package/npm/chart.js"></script>
+
     <div class="row">
         <div class="col-xl-3"></div>
         <div class="col colum_content_big">
             <h1 class="text-center nadestack_heading_one">Map Verteilung</h1>
-            <canvas id="horizontalBar"></canvas>
+            <canvas id="myChart" width="400" height="400"></canvas>
         </div>
         <div class="col-xl-3"></div>
     </div>
 @endsection
 
 <script>
-    $( document ).ready(function() {
-
-    new Chart(document.getElementById("horizontalBar"), {
-        "type": "horizontalBar",
-        "data": {
-            "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
-            "datasets": [{
-                "label": "My First Dataset",
-                "data": [22, 33, 55, 12, 86, 23, 14],
-                "fill": false,
-                "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)",
-                    "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)",
-                    "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)"
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
                 ],
-                "borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)",
-                    "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
-                "borderWidth": 1
+                borderWidth: 1
             }]
         },
-        "options": {
-            "scales": {
-                "xAxes": [{
-                    "ticks": {
-                        "beginAtZero": true
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
                     }
                 }]
             }
         }
-    });
-
     });
 </script>
