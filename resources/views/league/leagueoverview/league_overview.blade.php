6005eefdf31f8e98dbd3a5a5788fccaf28df8265
Eric <39589340+Mosman97@users.noreply.github.com>
2020-05-04 14:40:14 +0200

    league overview ist jetzt sick

--- resources/views/league/leagueoverview/league_overview.blade.php
+++ resources/views/league/leagueoverview/league_overview.blade.php
@@ -5,6 +5,34 @@
         <h1 class="text-center nadestack_heading_one nadestack-first-element"><img id='nav_logo' src='{{URL::asset('assets/img/logo.png')}}' width="60px"/>Nadestack League<img id='nav_logo' src='{{URL::asset('assets/img/logo.png')}}' width="60px"/></h1>
         <hr class="bg-light"/>
         <h2 class="text-center nadestack_heading_two">Make sure you are prepared</h2>
+        <div class="row" style="margin: 10px">
+            <div class="card-deck text-center">
+                <div class="card text-white bg-dark ">
+                    <div class="card-body">
+                        <i class="fa-3x fas fa-user-check" style="color: limegreen"></i>
+                        <p class="card-text" style="margin-top: 5px">Verify your ID at our third party partner</p>
+                    </div>
+                </div>
+                <div class="card text-white bg-dark ">
+                    <div class="card-body">
+                        <i class="fa-3x fas fa-book" style="color: limegreen"></i>
+                        <p class="card-text" style="margin-top: 5px">Read the rules</p>
+                    </div>
+                </div>
+                <div class="card text-white bg-dark " >
+                    <div class="card-body">
+                        <i class="fa-3x fas fa-signature" style="color: red"></i>
+                        <p class="card-text" style="margin-top: 5px">Join a team</p>
+                    </div>
+                </div>
+                <div class="card text-white bg-dark " >
+                    <div class="card-body">
+                        <i class="fa-3x fas fa-file-signature" style="color: red"></i>
+                        <p class="card-text" style="margin-top: 5px">Check in with your team</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <hr class="bg-light"/>
         <h2 class="text-center nadestack_heading_two">Time schedule - Season 1</h2>
         <div class="row">
@@ -37,7 +65,17 @@
                                 <i class="fas fa-sitemap"></i>
                             </div>
                             <h3 class="title">Ligaphase</h3>
-                            <p class="description">The leaue is starting and you are able to play now</p>
+                            <p class="description">The league is starting and you are able to play now</p>
+                        </a>
+                    </div>
+                    <div class="timeline">
+                        <a href="#" class="timeline-content">
+                            <span class="timeline-year">04.12.20</span>
+                            <div class="timeline-icon">
+                                <i class="fas fa-sort-amount-up-alt"></i>
+                            </div>
+                            <h3 class="title">Relegation</h3>
+                            <p class="description">Playoffs where you can try to get in higher divisions</p>
                         </a>
                     </div>
                 </div>
